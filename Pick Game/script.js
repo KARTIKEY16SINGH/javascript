d9c370772f53d8bfe558778da51b8213029faf03
Kartikey Singh <kartikeysingh16@gmail.com>
2022-01-08 21:33:03 +0530

    [Pick Game] Added functionality of dice roll and switching active player

--- Pick Game/script.js
+++ Pick Game/script.js
@@ -1,10 +1,40 @@
 'use strict';
 
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceImageEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const currentScore0El = document.getElementById('current--0');
+const currentScore1El = document.getElementById('current--1');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 
 diceImageEl.classList.add('hidden');
+
+let currentScore = 0;
+let activePlayer = 0;
+const scores = [0, 0];
+
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceImageEl.classList.remove('hidden');
+  diceImageEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    if (activePlayer == 0) currentScore0El.textContent = currentScore;
+    else currentScore1El.textContent = currentScore;
+  } else {
+    currentScore = 0;
+    if (activePlayer == 0) currentScore0El.textContent = currentScore;
+    else currentScore1El.textContent = currentScore;
+    activePlayer = !activePlayer;
+    player0El.classList.toggle('player--active')
+    player1El.classList.toggle('player--active')
+  }
+});
